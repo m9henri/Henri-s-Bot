9b52a501a6cbff4b6329f1ee050d0810aacbfcd0
Henri <97912678+m9henri@users.noreply.github.com>
2024-02-08 23:07:08 +0100

    changed ver 2.6.4 → 3.0.0

--- cogs/serverinfo.py
+++ cogs/serverinfo.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 # Variables
-version = "2.6.4"
+version = "3.0.0"
 emico = "https://media.discordapp.net/attachments/1179492325957840926/1190400494699552928/IMG_1124.png?ex=65a1a9da&is=658f34da&hm=cf80c472c2fb38010a58d69c13056a714134bdd590ed9e885a4af64b68126b83&=&format=webp&quality=lossless&width=640&height=662"
 name = "Henri's bot"
 
