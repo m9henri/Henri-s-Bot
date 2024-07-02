004b6824390d36f00c6d36eaa292ffc76674b5c9
avg newb programmer <dumby2008@icloud.com>
2024-07-02 07:55:46 -0500

    more rewritrten files

--- cogs/changelog.py
+++ cogs/changelog.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 # Variables
-version = "2.6.4"
+version = "3.0.0"
 emico = "https://media.discordapp.net/attachments/1179492325957840926/1190400494699552928/IMG_1124.png?ex=65a1a9da&is=658f34da&hm=cf80c472c2fb38010a58d69c13056a714134bdd590ed9e885a4af64b68126b83&=&format=webp&quality=lossless&width=640&height=662"
 name = "Henri's bot"
 
@@ -14,8 +14,7 @@ class changelog(commands.Cog):
     async def changelog(self, ctx):  # Added 'self' parameter
         em = discord.Embed(title="Changelog", description=f"{name} v{version}", color=discord.Colour.yellow())  # Fixed typo in 'Colour'
         em.set_author(name=name, icon_url=emico)
-        em.add_field(name="Moderation", value="`Message logs`")
-        em.add_field(name="Bot", value="`Better error handling, some small improvements, message logs, connection to database, new commands`")
+        em.add_field(name="Bot", value="`rewritten and optimized`")
         em.set_footer(text=f"{name} v{version}")
         await ctx.send(embed=em)
 
