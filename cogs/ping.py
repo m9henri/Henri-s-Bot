f18e9132ae57a46200a256dd58cb9b7b269fc8e6
avg newb programmer <dumby2008@icloud.com>
2024-07-02 15:20:19 -0500

    Update ping.py
    comment

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+# variables
 emico = "https://cdn.discordapp.com/attachments/1256069941132529674/1257792355151515658/OIP.png?ex=6685b1ea&is=6684606a&hm=b0ccde489363500a4f6593c652825230917f95042411e7ae7ec491e02f292ba5&"
 name = "latebncy"
 
@@ -13,11 +14,11 @@ class Ping(commands.Cog):
         embed = discord.Embed(title="Pong!", color=discord.Color.blue())
         em.set_author(name=name, icon_url=emico)
 
-        # Získání latence bota
+        # set bot side latency
         latency = round(self.bot.latency * 1000, 2)
         embed.add_field(name="Bot Latency", value=f"{latency}ms", inline=False)
 
-        # Získání latence zprávy
+        # set message latency
         msg_latency = round((ctx.message.created_at - ctx.message.created_at).total_seconds() * 1000, 2)
         embed.add_field(name="Message Latency", value=f"{msg_latency}ms", inline=False)
 
