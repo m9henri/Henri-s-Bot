35208c3d420be37a8425a9a91cb6f06c2fbd533d
avg newb programmer <dumby2008@icloud.com>
2024-07-02 15:18:54 -0500

    "rewrite" ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,13 +1,17 @@
 import discord
 from discord.ext import commands
 
+emico = "https://cdn.discordapp.com/attachments/1256069941132529674/1257792355151515658/OIP.png?ex=6685b1ea&is=6684606a&hm=b0ccde489363500a4f6593c652825230917f95042411e7ae7ec491e02f292ba5&"
+name = "latebncy"
+
 class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="ping", help="Show ping information.")
     async def ping(self, ctx):
-        embed = discord.Embed(title="Ping Information", color=discord.Color.blue())
+        embed = discord.Embed(title="Pong!", color=discord.Color.blue())
+        em.set_author(name=name, icon_url=emico)
 
         # Získání latence bota
         latency = round(self.bot.latency * 1000, 2)
