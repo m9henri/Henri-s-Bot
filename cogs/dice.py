27f22b4db78adb99f0202b14d13e11ee2bd92480
avg newb programmer <dumby2008@icloud.com>
2024-07-03 14:16:44 -0500

    Optimized and bug fixed dice.py
    Fixed being able to roll 999 dice

--- cogs/dice.py
+++ cogs/dice.py
@@ -5,13 +5,16 @@ import random
 class dice(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    @commands.command(name="dice", help="random number")
+    @commands.command(name="dice", help="less than 10 random numbers at a command")
     async def dice(self, ctx, amount: int, size: int):
-        n = 1
-        while n <= amount:
-            result = random.randint(1,size)
-            await ctx.send("the result is " + str(result))
-            n = (n + 1)
+        if amount <= 10:
+            while n <= amount:
+                n = 1
+                result = random.randint(1,size)
+                await ctx.send("the result is " + str(result))
+                n += 1
+            elif amount > 10:
+                ctx.send("ran into error: too many dice")
 
 
 
