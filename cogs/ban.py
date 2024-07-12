74f4aa434fac31291a7b4bbc4b35158a43dd85ab
avg newb programmer <dumby2008@icloud.com>
2024-07-12 15:51:11 -0500

    Add files via upload

--- cogs/ban.py
+++ cogs/ban.py
@@ -7,6 +7,7 @@ class ban(commands.Cog):
     @commands.command(name="ban", help="ban member")
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member: discord.Member, reason: str):        
+        reason = reason or "no reason provided"
         await member.ban(reason=reason)
         await ctx.send(f"User {member.mention} has been banned.")
         await ctz.send(f"https://tenor.com/view/thor-banned-discord-hammer-mjolnir-gif-1994015921512866670")
--- cogs/dice.py
+++ cogs/dice.py
@@ -2,20 +2,19 @@ import discord, random
 from discord.ext import commands
 
 class dice(commands.Cog):
-    
+    def __init__(self, bot):
+        self.bot = bot
     @commands.command(name="dice", help="random number")
     async def dice(self, ctx, amount: int, size: int):
-        if amount <= 10:
-            n = 1
-            while n <= amount:    
-                result = random.randint(1,size)
-                await ctx.send(f"result of dice {n} is {result}")
-                n += 1
-        elif not amount <= 10:
-            await ctx.send("ran into error: too many dice, try 10 or less")
+        n = 1
+        while n <= amount:    
+            result = random.randint(1, size)
+            send = (f"result of dice {n} is ,")
+            result2 = send.append(f"{result}, ")
+            n += 1
+        ctx.send(send)
 
-def __init__(self, bot):
-    self.bot = bot
+
 
 async def setup(bot):
     await bot.add_cog(dice(bot))
