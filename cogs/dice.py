8ee9a966b5e44ed1d58626fdea29498ae2a68dbc
avg newb programmer <dumby2008@icloud.com>
2024-07-12 16:02:25 -0500

    grammar

--- cogs/dice.py
+++ cogs/dice.py
@@ -9,7 +9,7 @@ class dice(commands.Cog):
         n = 1
         while n <= amount:    
             num = random.randint(1, size)
-            send = (f"result of dice {n} is ,")
+            send = (f"result of dice {n} is/are,")
             result = send.append(f"{num}, ")
             n += 1
         ctx.send(send)
