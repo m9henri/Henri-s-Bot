907068f9fdc7fe90526ee9d0620bda46820494de
avg newb programmer <dumby2008@icloud.com>
2024-08-26 16:40:59 -0500

    the bot is homosexual, and it likes men

--- cogs/dice.py
+++ cogs/dice.py
@@ -1,17 +1,17 @@
+import discord, random
 import random
 from discord.ext import commands
 
 class dice(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-    @commands.command(name="dice", help="random number")
     async def dice(self, ctx, amount: int, size: int):
-        str: number = f'the result of the {amount} dice is: ' 
         n = 1
-        while n <= amount:    
-            num = random.randint(1, size)
-            number += f" {num}, "
-            n += 1
-            await ctx.send({number})
+        if amount <10:
+            while n <= amount:    
+                num = random.randint(1, size)
+                send = (f"results of the dice is/are: ")
+                send += (f"{num}, ")
+                n += 1
+            await ctx.send({f"results of the dice: {send}"})
+
 async def setup(bot):
     await bot.add_cog(dice(bot))
