b7084186245af935c564fa1232a7ed87651a6ce9
avg newb programmer <dumby2008@icloud.com>
2024-08-26 16:25:50 -0500

    솝

--- cogs/dice.py
+++ cogs/dice.py
@@ -6,13 +6,12 @@ class dice(commands.Cog):
         self.bot = bot
     @commands.command(name="dice", help="random number")
     async def dice(self, ctx, amount: int, size: int):
+        str: number = f'the result of the {amount} dice is: ' 
         n = 1
         while n <= amount:    
             num = random.randint(1, size)
-            str: number = f" {num}, "
-            if n != 1:
-                number += f"{num}, "
-            await ctx.send({f"results of the dice: {number}"})
+            number += f" {num}, "
             n += 1
+            await ctx.send({{number}"})
 async def setup(bot):
     await bot.add_cog(dice(bot))
